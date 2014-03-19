7369aeaa6ad9c41b76cf939ac5fe8fd3e8907c2c
kpartlow <kpartlow@gmail.com>
2014-03-19 08:19:34 -0400

    Added in JsonFormatter

---
+++ src/test/java/com/cedarsoftware/ncube/TestJsonFormatter.java
@@ -0,0 +1,28 @@
+package com.cedarsoftware.ncube;
+
+import com.cedarsoftware.util.io.JsonWriter;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by kpartlow on 3/18/14.
+ */
+public class TestJsonFormatter {
+
+    @Test
+    public void testFormatter() throws Exception {
+        NCube ncube = NCubeManager.getNCubeFromResource("simpleJsonArrayTest.json");
+
+        System.out.println(JsonWriter.objectToJson(ncube));
+
+        System.out.println("\n------------------\n");
+        System.out.println(ncube.toFormattedJson());
+
+    }
+}

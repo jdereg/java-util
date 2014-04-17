c0e2302e178b15915ed894223599648353ab29e7
jdereg <jdereg@gmail.com>
2014-04-17 19:34:13 -0400

    - added missing import

--- src/test/java/com/cedarsoftware/ncube/TestNCube.java
+++ src/test/java/com/cedarsoftware/ncube/TestNCube.java
@@ -40,6 +40,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 

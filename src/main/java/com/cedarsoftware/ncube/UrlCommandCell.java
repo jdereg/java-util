f77d66f9034aa4d46e75b3d81eff6b5c3039e2c0
kpartlow <kpartlow@gmail.com>
2014-08-02 23:13:44 -0400

    Added tests For CachingInputStream

--- src/main/java/com/cedarsoftware/ncube/UrlCommandCell.java
+++ src/main/java/com/cedarsoftware/ncube/UrlCommandCell.java
@@ -585,7 +585,7 @@ public abstract class UrlCommandCell implements CommandCell
         }
     }
 
-    private static class CachingInputStream extends FilterInputStream
+    static class CachingInputStream extends FilterInputStream
     {
         ByteArrayOutputStream cache = new ByteArrayOutputStream();
 

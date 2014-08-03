9c12840d9c1e8be17ee878b3d30764aa45848d64
kpartlow <kpartlow@gmail.com>
2014-08-02 23:02:56 -0400

    Added some tests for CdnUrlClassLoader

--- src/test/java/com/cedarsoftware/ncube/util/TestCdnClassLoader.java
+++ src/test/java/com/cedarsoftware/ncube/util/TestCdnClassLoader.java
@@ -3,6 +3,8 @@ package com.cedarsoftware.ncube.util;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -23,11 +25,27 @@ public class TestCdnClassLoader
 
         CdnClassLoader testLoader2 = new CdnClassLoader(TestCdnClassLoader.class.getClassLoader(), false, false);
         assertTrue(testLoader2.isLocalOnlyResource("org.codehaus.groovy.transform.ASTTransformation"));
-        assertTrue(testLoader2.isLocalOnlyResource("ncube/grv/exp/GroovyExpression"));
-        assertTrue(testLoader2.isLocalOnlyResource("ncube/grv/method/GroovyMethod"));
+        assertTrue(testLoader2.isLocalOnlyResource("ncube/grv/exp/NCubeGroovyExpression"));
+        assertTrue(testLoader2.isLocalOnlyResource("ncube/grv/method/NCubeGroovyController"));
         assertFalse(testLoader2.isLocalOnlyResource("FooBeanInfo.groovy"));
         assertFalse(testLoader2.isLocalOnlyResource("FooCustomizer.groovy"));
         assertTrue(testLoader2.isLocalOnlyResource("AnyThing.class"));
         assertFalse(testLoader2.isLocalOnlyResource("AnyThing.groovy"));
+    }
+
+    @Test
+    public void testGetResource() {
+        CdnClassLoader testLoader1 = new CdnClassLoader(TestCdnClassLoader.class.getClassLoader(), true, true);
+        assertNotNull(testLoader1.getResource("cdnRouter.json"));
+        assertNull(testLoader1.getResource("ncube/grv/method/NCubeGroovyController.class"));
+        assertNotNull(TestCdnClassLoader.class.getClassLoader().getResource("ncube/grv/method/NCubeGroovyController.class"));
+    }
+
+    @Test
+    public void testGetResources() throws Exception {
+        CdnClassLoader testLoader1 = new CdnClassLoader(TestCdnClassLoader.class.getClassLoader(), true, true);
+        assertTrue(testLoader1.getResources("cdnRouter.json").hasMoreElements());
+        assertFalse(testLoader1.getResources("ncube/grv/method/NCubeGroovyController.class").hasMoreElements());
+        assertTrue(TestCdnClassLoader.class.getClassLoader().getResources("ncube/grv/method/NCubeGroovyController.class").hasMoreElements());
     }
 }

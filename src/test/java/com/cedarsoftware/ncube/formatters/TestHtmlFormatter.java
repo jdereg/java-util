2ab1d9bfe192a881ea98a31749c1f1fe29e55541
kpartlow <kpartlow@gmail.com>
2014-08-15 17:46:02 -0400

    Added some html formatter tests

--- src/test/java/com/cedarsoftware/ncube/formatters/TestHtmlFormatter.java
+++ src/test/java/com/cedarsoftware/ncube/formatters/TestHtmlFormatter.java
@@ -3,7 +3,6 @@ package com.cedarsoftware.ncube.formatters;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 /**
  * Created by ken on 8/15/2014.
@@ -15,6 +14,7 @@ public class TestHtmlFormatter {
         assertEquals("null", HtmlFormatter.getCellValueAsString(null));
         assertEquals("foo", HtmlFormatter.getCellValueAsString("foo"));
         assertEquals("[0, 1, 2, 3]", HtmlFormatter.getCellValueAsString(new int[] { 0, 1, 2, 3}));
-        assertEquals("[true, false]", HtmlFormatter.getCellValueAsString(new Boolean[] { Boolean.TRUE, Boolean.FALSE}));
+        assertEquals("[true, false]", HtmlFormatter.getCellValueAsString(new Object[] { Boolean.TRUE, Boolean.FALSE}));
+        assertEquals("5.0", HtmlFormatter.getCellValueAsString(5.0));
     }
 }

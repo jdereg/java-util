8da1e6a61cd666c1fcb4a3f88e7165347f608d9e
kpartlow <kpartlow@gmail.com>
2014-08-29 10:28:51 -0400

    More TestWriter and TestParser tests

--- src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestParser.java
+++ src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestParser.java
@@ -1,5 +1,6 @@
 package com.cedarsoftware.ncube.formatters;
 
+import com.cedarsoftware.ncube.NCube;
 import com.cedarsoftware.ncube.NCubeManager;
 import com.cedarsoftware.ncube.NCubeTest;
 import com.cedarsoftware.ncube.TestNCube;
@@ -63,6 +64,22 @@ public class TestNCubeTestParser
         assertEquals("test3", dto.getName());
         assertEquals(true, dto.getCoordinate().get("coord1"));
         assertEquals(6, dto.getCoordinate().get("coord2"));
+
+    }
+
+    @Test
+    public void testStringIds() {
+        NCube ncube = NCubeManager.getNCubeFromResource("stringIds.json");
+        List<NCubeTest> generatedTests = ncube.generateNCubeTests();
+
+
+        List<NCubeTest> tests = TestNCubeTestParser.getTestsFromResource("stringIds-test-data.json");
+
+        for (NCubeTest test: tests)
+        {
+            Object o = ncube.getCell(test.getCoordinate());
+            assertEquals(o, test.getExpectedResult());
+        }
 
     }
 
--- src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestWriter.java
+++ src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestWriter.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by kpartlow on 8/27/2014.
@@ -17,15 +18,22 @@ public class TestNCubeTestWriter
     @Test
     public void test() throws Exception {
         NCube ncube = NCubeManager.getNCubeFromResource("stringIds.json");
+        List<NCubeTest> generatedTests = ncube.generateNCubeTests();
 
-        //List<NCubeTest> tests = TestNCubeTestParser.getTestsFromResource("stringIds-test-data.json");
 
-        List<NCubeTest> tests = ncube.generateNCubeTests();
+        List<NCubeTest> tests = TestNCubeTestParser.getTestsFromResource("stringIds-test-data.json");
+
+
         for (NCubeTest test: tests) {
             Object o = ncube.getCell(test.getCoordinate());
             assertEquals(o, test.getExpectedResult());
         }
 
-        System.out.println(new NCubeTestWriter().write(tests));
+
+    }
+
+    @Test
+    public void testNullWrite() throws Exception {
+        assertNull(new NCubeTestWriter().write(null));
     }
 }

b01b9c50dde95b3d26259c4137b327d109cbcd11
kpartlow <kpartlow@gmail.com>
2014-08-29 15:33:21 -0400

    Updated TestWriterTests

--- src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestWriter.java
+++ src/test/java/com/cedarsoftware/ncube/formatters/TestNCubeTestWriter.java
@@ -3,12 +3,14 @@ package com.cedarsoftware.ncube.formatters;
 import com.cedarsoftware.ncube.NCube;
 import com.cedarsoftware.ncube.NCubeManager;
 import com.cedarsoftware.ncube.NCubeTest;
+import com.cedarsoftware.util.DeepEquals;
 import org.junit.Test;
 
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by kpartlow on 8/27/2014.
@@ -16,7 +18,7 @@ import static org.junit.Assert.assertNull;
 public class TestNCubeTestWriter
 {
     @Test
-    public void test() throws Exception {
+    public void testWriting() throws Exception {
         NCube ncube = NCubeManager.getNCubeFromResource("stringIds.json");
         List<NCubeTest> generatedTests = ncube.generateNCubeTests();
 
@@ -29,6 +31,11 @@ public class TestNCubeTestWriter
             assertEquals(o, test.getExpectedResult());
         }
 
+        String s = new NCubeTestWriter().write(tests);
+        List<NCubeTest> repop = new NCubeTestParser().parse(s);
+
+        assertEquals(tests.size(), repop.size());
+        assertTrue(DeepEquals.deepEquals(tests, repop));
 
     }
 

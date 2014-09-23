8910863f916fab452eea9587d259c4d102ab9b3a
kpartlow <kpartlow@gmail.com>
2014-09-23 12:52:25 -0400

    Increased Code Coverage on CellInfo and CellTypes

---
+++ src/test/java/com/cedarsoftware/ncube/TestCellInfo.java
@@ -0,0 +1,72 @@
+package com.cedarsoftware.ncube;
+
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Calendar;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by kpartlow on 9/23/2014.
+ */
+public class TestCellInfo
+{
+
+    @Test
+    public void testFormatForEditing() {
+        assertEquals("4.56", CellInfo.formatForEditing(4.56));
+        assertEquals("0.0", CellInfo.formatForEditing(0.0));
+        assertEquals("4.0", CellInfo.formatForEditing(new Float(4)));
+        assertEquals("4.0", CellInfo.formatForEditing(new Double(4)));
+
+        assertEquals("4.56", CellInfo.formatForEditing(new BigDecimal("4.56000")));
+        assertEquals("4.56", CellInfo.formatForEditing(new BigDecimal("4.56")));
+    }
+
+    @Test
+    public void testFormatForDisplay() {
+        assertEquals("4.56", CellInfo.formatForEditing(4.560));
+        assertEquals("4.5", CellInfo.formatForEditing(4.5));
+
+        assertEquals("4.56", CellInfo.formatForEditing(new BigDecimal("4.5600")));
+        assertEquals("4", CellInfo.formatForEditing(new BigDecimal("4.00")));
+        assertEquals("4", CellInfo.formatForEditing(new BigDecimal("4")));
+    }
+
+    @Test
+    public void testRecreate() {
+        assertNull(new CellInfo(null).recreate());
+
+        performRecreateAssertion(new StringUrlCmd("http://www.google.com", true));
+        performRecreateAssertion(new Double(4.56));
+        performRecreateAssertion(new Float(4.56));
+        performRecreateAssertion(new Short((short)4));
+        performRecreateAssertion(new Long(4));
+        performRecreateAssertion(new Integer(4));
+        performRecreateAssertion(new Byte((byte)4));
+        performRecreateAssertion(new BigDecimal("4.56"));
+        performRecreateAssertion(new BigInteger("900"));
+        performRecreateAssertion(Boolean.TRUE);
+        performRecreateAssertion(new GroovyExpression("0", null));
+        performRecreateAssertion(new GroovyMethod("0", null));
+        performRecreateAssertion(new GroovyTemplate(null, "http://www.google.com", false));
+        performRecreateAssertion(new BinaryUrlCmd("http://www.google.com", false));
+        performArrayRecreateAssertion(new byte[]{0, 4, 5, 6});
+        performRecreateAssertion("foo");
+
+        performRecreateAssertion(Calendar.getInstance().getTime());
+    }
+
+    public void performRecreateAssertion(Object o) {
+        assertEquals(o, new CellInfo(o).recreate());
+    }
+
+    public void performArrayRecreateAssertion(byte[] o) {
+        assertArrayEquals(o, (byte[])new CellInfo(o).recreate());
+    }
+
+}
--- src/test/java/com/cedarsoftware/ncube/TestCellTypes.java
+++ src/test/java/com/cedarsoftware/ncube/TestCellTypes.java
@@ -31,4 +31,28 @@ public class TestCellTypes
         assertEquals("double", CellTypes.getType(Double.MAX_VALUE, "cells"));
         assertEquals("float", CellTypes.getType(Float.MAX_VALUE, "cells"));
     }
+
+    @Test
+    public void testGetTypeFromString() {
+        assertEquals(CellTypes.String, CellTypes.getTypeFromString(null));
+        assertEquals(CellTypes.String, CellTypes.getTypeFromString("string"));
+        assertEquals(CellTypes.Date, CellTypes.getTypeFromString("date"));
+        assertEquals(CellTypes.Boolean, CellTypes.getTypeFromString("boolean"));
+        assertEquals(CellTypes.Byte, CellTypes.getTypeFromString("byte"));
+        assertEquals(CellTypes.Short, CellTypes.getTypeFromString("short"));
+        assertEquals(CellTypes.Integer, CellTypes.getTypeFromString("int"));
+        assertEquals(CellTypes.Long, CellTypes.getTypeFromString("long"));
+        assertEquals(CellTypes.Float, CellTypes.getTypeFromString("float"));
+        assertEquals(CellTypes.Double, CellTypes.getTypeFromString("double"));
+        assertEquals(CellTypes.BigDecimal, CellTypes.getTypeFromString("bigdec"));
+        assertEquals(CellTypes.BigInteger, CellTypes.getTypeFromString("bigint"));
+        assertEquals(CellTypes.Binary, CellTypes.getTypeFromString("binary"));
+        assertEquals(CellTypes.Exp, CellTypes.getTypeFromString("exp"));
+        assertEquals(CellTypes.Method, CellTypes.getTypeFromString("method"));
+        assertEquals(CellTypes.Template, CellTypes.getTypeFromString("template"));
+        assertEquals(CellTypes.LatLon, CellTypes.getTypeFromString("latlon"));
+        assertEquals(CellTypes.Point2D, CellTypes.getTypeFromString("point2d"));
+        assertEquals(CellTypes.Point3D, CellTypes.getTypeFromString("point3d"));
+    }
+
 }

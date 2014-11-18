62eff25eddb05147775c46cadf49c3b8d58152a7
jdereg <jdereg@gmail.com>
2014-11-17 22:36:20 -0500

    - Links in n-cube HTML are now click-able, even when they are a relative URL (the server resolves the relative URL and returns the absolute URL).

--- src/main/java/com/cedarsoftware/ncube/NCubeManager.java
+++ src/main/java/com/cedarsoftware/ncube/NCubeManager.java
@@ -1,7 +1,12 @@
 package com.cedarsoftware.ncube;
 
 import com.cedarsoftware.ncube.util.CdnClassLoader;
-import com.cedarsoftware.util.*;
+import com.cedarsoftware.util.ArrayUtilities;
+import com.cedarsoftware.util.CaseInsensitiveSet;
+import com.cedarsoftware.util.IOUtilities;
+import com.cedarsoftware.util.MapUtilities;
+import com.cedarsoftware.util.StringUtilities;
+import com.cedarsoftware.util.SystemUtilities;
 import com.cedarsoftware.util.io.JsonObject;
 import com.cedarsoftware.util.io.JsonReader;
 import com.cedarsoftware.util.io.JsonWriter;
@@ -10,10 +15,20 @@ import ncube.grv.method.NCubeGroovyController;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
@@ -770,8 +785,8 @@ public class NCubeManager
         {
             return "No class loader exists for this app";
         }
-        URL fullyQualifiedUrl = gcl.getResource(relativeUrl);
-        return fullyQualifiedUrl.toString();
+        URL absUrl = gcl.getResource(relativeUrl);
+        return absUrl != null ? absUrl.toString() : null;
     }
 
     // ----------------------------------------- Resource APIs ---------------------------------------------------------

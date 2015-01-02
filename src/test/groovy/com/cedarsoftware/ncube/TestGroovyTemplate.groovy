abdaf6e289d488475e194f4d30291a75a0427b27
jdereg <jdereg@gmail.com>
2015-01-01 22:49:53 -0500

    - used static import for asserts

--- src/test/groovy/com/cedarsoftware/ncube/TestGroovyTemplate.groovy
+++ src/test/groovy/com/cedarsoftware/ncube/TestGroovyTemplate.groovy
@@ -1,13 +1,29 @@
 package com.cedarsoftware.ncube
 
-import org.junit.Assert
 import org.junit.Test
 
 import java.lang.reflect.Constructor
 import java.lang.reflect.Modifier
 
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertNotNull
+
 /**
- * Created by kpartlow on 11/18/2014.
+ * @author John DeRegnaucourt (jdereg@gmail.com)
+ *         <br/>
+ *         Copyright (c) Cedar Software LLC
+ *         <br/><br/>
+ *         Licensed under the Apache License, Version 2.0 (the 'License')
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br/><br/>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br/><br/>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an 'AS IS' BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
  */
 public class TestGroovyTemplate
 {
@@ -16,9 +32,9 @@ public class TestGroovyTemplate
     {
         Class c = GroovyTemplate.class
         Constructor<GroovyTemplate> con = c.getDeclaredConstructor()
-        Assert.assertEquals Modifier.PRIVATE, con.modifiers & Modifier.PRIVATE
+        assertEquals Modifier.PRIVATE, con.modifiers & Modifier.PRIVATE
         con.accessible = true
-        Assert.assertNotNull con.newInstance()
+        assertNotNull con.newInstance()
     }
 
 }

8d82bec0a6dfa491b6dab0d22186e5236bf432b5
kpartlow <kpartlow@gmail.com>
2015-01-08 10:15:24 -0500

    Added threaded test for clearCache() and clearCache(appId)

--- src/test/groovy/com/cedarsoftware/ncube/TestThreadedClearCache.groovy
+++ src/test/groovy/com/cedarsoftware/ncube/TestThreadedClearCache.groovy
@@ -27,6 +27,7 @@ public class TestThreadedClearCache
 {
     public static String USER_ID = TestNCubeManager.USER_ID
     public static ApplicationID appId = new ApplicationID(ApplicationID.DEFAULT_TENANT, "clearCacheTest", ApplicationID.DEFAULT_VERSION, ReleaseStatus.SNAPSHOT.name())
+    public static ApplicationID appId2 = new ApplicationID(ApplicationID.DEFAULT_TENANT, "clearCacheTest2", ApplicationID.DEFAULT_VERSION, ReleaseStatus.SNAPSHOT.name())
 
     private TestingDatabaseManager manager;
 
@@ -52,35 +53,133 @@ public class TestThreadedClearCache
 
         // add cubes for this test.
         manager.addCubes(appId, USER_ID, ncubes)
+        manager.addCubes(appId2, USER_ID, ncubes)
 
         concurrencyTest();
 
         // remove cubes
-
-        NCube cube = NCubeManager.getCube(appId, "MathController")
-
-        def input = [:]
-        input.env = "a"
-        input.x = 5
-        input.method = 'square'
-
-        assertEquals(25, cube.getCell(input))
-
-        input.method = 'factorial'
-        assertEquals(120, cube.getCell(input))
-
-        input.env = "b"
-        input.x = 6
-        input.method = 'square'
-        assertEquals(6, cube.getCell(input))
-        input.method = 'factorial'
-        assertEquals(6, cube.getCell(input))
+        manager.removeCubes(appId, USER_ID, ncubes);
+        manager.removeCubes(appId2, USER_ID, ncubes);
+    }
 
 
+    @Test
+    public void testCubesWithThreadedClearCacheWithAppId() throws Exception {
+        NCube[] ncubes = TestingDatabaseHelper.getCubesFromDisk("sys.classpath.2per.app.json", "math.controller.json");
+
+        // add cubes for this test.
+        manager.addCubes(appId, USER_ID, ncubes)
+
+        concurrencyTestWithAppId();
+
+        // remove cubes
         manager.removeCubes(appId, USER_ID, ncubes);
     }
 
-    private static void concurrencyTest() throws IOException
+    private void concurrencyTest() throws Exception
+    {
+        def firstApp =
+                {
+                    long start = System.currentTimeMillis()
+                    while (System.currentTimeMillis() - start < 3000) {
+                        for (int j = 0; j < 100; j++) {
+                            NCube cube = NCubeManager.getCube(appId, "MathController")
+
+                            def input = [:]
+                            input.env = "a"
+                            input.x = 5
+                            input.method = 'square'
+
+                            assertEquals(25, cube.getCell(input))
+
+                            input.method = 'factorial'
+                            assertEquals(120, cube.getCell(input))
+
+                            input.env = "b"
+                            input.x = 6
+                            input.method = 'square'
+                            assertEquals(6, cube.getCell(input))
+                            input.method = 'factorial'
+                            assertEquals(6, cube.getCell(input))
+                        }
+                    }
+                }
+
+        def secondApp =
+                {
+                    long start = System.currentTimeMillis()
+                    while (System.currentTimeMillis() - start < 3000) {
+                        for (int j = 0; j < 100; j++) {
+                            NCube cube = NCubeManager.getCube(appId, "MathController")
+
+                            def input = [:]
+                            input.env = "a"
+                            input.x = 5
+                            input.method = 'square'
+
+                            assertEquals(25, cube.getCell(input))
+
+                            input.method = 'factorial'
+                            assertEquals(120, cube.getCell(input))
+
+                            input.env = "b"
+                            input.x = 6
+                            input.method = 'square'
+                            assertEquals(6, cube.getCell(input))
+                            input.method = 'factorial'
+                            assertEquals(6, cube.getCell(input))
+                        }
+                    }
+                }
+
+        def clearCache = {
+            long start = System.currentTimeMillis()
+            while (System.currentTimeMillis() - start < 3000) {
+                NCubeManager.clearCache();
+            }
+        }
+
+
+        Thread[] firstAppThreads = new Thread[8]
+        Thread[] secondAppThreads = new Thread[8]
+
+        for (int i = 0; i < 8; i++)
+        {
+            firstAppThreads[i] = new Thread(firstApp);
+            firstAppThreads[i].name = 'FirstApp' + i
+            firstAppThreads[i].daemon = true
+
+            secondAppThreads[i] = new Thread(secondApp);
+            secondAppThreads[i].name = 'SecondApp' + i
+            secondAppThreads[i].daemon = true
+        }
+
+        Thread clear = new Thread(clearCache);
+        clear.name = "ClearCache";
+        clear.daemon = true;
+
+        // Start all at the same time (more concurrent than starting them during construction)
+        for (int i = 0; i < 8; i++)
+        {
+            firstAppThreads[i].start()
+            secondAppThreads[i].start()
+        }
+        clear.start();
+
+        for (int i = 0; i < 8; i++)
+        {
+            try
+            {
+                firstAppThreads[i].join()
+                secondAppThreads[i].join()
+            }
+            catch (InterruptedException ignored)
+            { }
+        }
+        clear.join();
+    }
+
+    private void concurrencyTestWithAppId() throws Exception
     {
         def run =
         {
@@ -142,10 +241,10 @@ public class TestThreadedClearCache
             try
             {
                 threads[i].join()
-                clear.join();
             }
             catch (InterruptedException ignored)
             { }
         }
+        clear.join();
     }
 }

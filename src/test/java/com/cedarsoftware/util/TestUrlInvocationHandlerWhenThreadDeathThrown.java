c7f5089b49d429322264d9eee3a410e4e8887232
jdereg <jdereg@gmail.com>
2015-02-23 10:45:50 -0500

    - removed test dependency on json-io

--- src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWhenThreadDeathThrown.java
+++ src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWhenThreadDeathThrown.java
@@ -1,7 +1,5 @@
 package com.cedarsoftware.util;
 
-import com.cedarsoftware.util.io.JsonReader;
-import com.cedarsoftware.util.io.JsonWriter;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,7 +8,6 @@ import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.net.HttpURLConnection;
@@ -52,7 +49,7 @@ public class TestUrlInvocationHandlerWhenThreadDeathThrown
     /**
      * Created by kpartlow on 5/11/2014.
      */
-    private class UrlInvocationHandlerJsonStrategy implements UrlInvocationHandlerStrategy
+    private static class UrlInvocationHandlerJsonStrategy implements UrlInvocationHandlerStrategy
     {
         private String _url;
         Map _store = new HashMap();
@@ -62,31 +59,26 @@ public class TestUrlInvocationHandlerWhenThreadDeathThrown
             _url = url;
         }
 
-        @Override
         public URL buildURL(Object proxy, Method m, Object[] args) throws MalformedURLException
         {
             return new URL(_url);
         }
 
-        @Override
         public int getRetryAttempts()
         {
             return 0;
         }
 
-        @Override
         public void getCookies(URLConnection c)
         {
             UrlUtilities.getCookies(c, null);
         }
 
-        @Override
         public void setRequestHeaders(URLConnection c)
         {
 
         }
 
-        @Override
         public void setCookies(URLConnection c)
         {
             try
@@ -97,31 +89,14 @@ public class TestUrlInvocationHandlerWhenThreadDeathThrown
             }
         }
 
-        @Override
         public byte[] generatePostData(Object proxy, Method m, Object[] args) throws IOException
         {
-            ByteArrayOutputStream ba_out = new ByteArrayOutputStream();
-            JsonWriter jwr = new JsonWriter(ba_out);
-            jwr.write(new Object[]{m.getName(), args});
-            IOUtilities.close(jwr);
-
-            return ba_out.toByteArray();
+            return "[\"foo\",null]".getBytes();
         }
 
         public Object readResponse(URLConnection c) throws IOException
         {
-            JsonReader reader = null;
-
-            try
-            {
-                reader = new JsonReader(IOUtilities.getInputStream(c));
-                Object[] res = (Object[]) reader.readObject();
-                return res[0];
-            }
-            finally
-            {
-                IOUtilities.close(reader);
-            }
+            return null;
         }
     }
 
--- src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWithPlainReader.java
+++ src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWithPlainReader.java
@@ -1,20 +1,20 @@
 package com.cedarsoftware.util;
 
-import com.cedarsoftware.util.io.JsonReader;
-import com.cedarsoftware.util.io.JsonWriter;
+import com.google.gson.Gson;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 
+import static org.junit.Assert.fail;
 
 
 /**
@@ -26,32 +26,49 @@ public class TestUrlInvocationHandlerWithPlainReader
 
     @Test
     public void testWithBadUrl() {
-        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class, new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://cedarsoftware.com/invalid/url", "F012982348484444")));
+        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class,
+                new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://cedarsoftware.com/invalid/url", "F012982348484444")));
         Assert.assertNull(item.foo());
     }
 
     @Test
     public void test() {
-        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class, new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-test.json", "F012982348484444")));
+        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class,
+                new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-test.json", "F012982348484444")));
         Assert.assertEquals("test-passed", item.foo());
     }
 
     @Test
     public void testWithSessionAwareInvocationHandler() {
-        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class, new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-test.json", "F012982348484444")));
+        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class,
+                new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-test.json", "F012982348484444")));
         Assert.assertEquals("test-passed", item.foo());
     }
 
     @Test
     public void testUrlInvocationHandlerWithException() {
-        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class, new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-exception.json", "F012982348484444")));
-        Assert.assertNull(item.foo());
+        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class,
+                new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-exception.json", "F012982348484444")));
+        try
+        {
+            item.foo();
+            fail();
+        }
+        catch (ClassCastException ignored)
+        { }
     }
 
     @Test
     public void testUrlInvocationHandlerWithNonInvocationException() {
-        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class, new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-non-invocation-exception.json", "F012982348484444")));
-        Assert.assertNull(item.foo());
+        TestUrlInvocationInterface item = ProxyFactory.create(TestUrlInvocationInterface.class,
+                new UrlInvocationHandler(new UrlInvocationHandlerJsonStrategy("http://www.cedarsoftware.com/tests/java-util/url-invocation-handler-non-invocation-exception.json", "F012982348484444")));
+        try
+        {
+            item.foo();
+            fail();
+        }
+        catch (ClassCastException ignored)
+        { }
     }
 
 
@@ -75,72 +92,44 @@ public class TestUrlInvocationHandlerWithPlainReader
             _sessionId = sessionId;
         }
 
-        @Override
         public URL buildURL(Object proxy, Method m, Object[] args) throws MalformedURLException
         {
             return new URL(_url);
         }
 
-        @Override
         public int getRetryAttempts()
         {
             return 0;
         }
 
-        @Override
         public void getCookies(URLConnection c)
         {
         }
 
-        @Override
         public void setRequestHeaders(URLConnection c)
         {
 
         }
 
-        @Override
         public void setCookies(URLConnection c)
         {
             c.setRequestProperty("Cookie", "JSESSIONID=" + _sessionId);
         }
 
-        @Override
         public byte[] generatePostData(Object proxy, Method m, Object[] args) throws IOException
         {
-            ByteArrayOutputStream ba_out = new ByteArrayOutputStream();
-            JsonWriter jwr = new JsonWriter(ba_out);
-            jwr.write(new Object[]{m.getName(), args});
-            IOUtilities.close(jwr);
-
-            if (LOG.isDebugEnabled())
-            {    // DEBUG
-                String jsonCall = new String(ba_out.toByteArray(), "UTF-8");
-                LOG.debug("Calling server:\n    " + jsonCall);
-            }
-
-            return ba_out.toByteArray();
+            return "[\"foo\",null]".getBytes("UTF-8");
         }
 
         public Object readResponse(URLConnection c) throws IOException
         {
-            JsonReader reader = null;
+            Reader reader = null;
 
             try
             {
-                if (LOG.isDebugEnabled())
-                {
-                    ByteArrayOutputStream input = new ByteArrayOutputStream(32768);
-                    IOUtilities.transfer(IOUtilities.getInputStream(c), input);
-                    byte[] bytes = input.toByteArray();
-                    String jsonResp = new String(bytes, "UTF-8");
-                    LOG.debug(jsonResp);
-                    reader = new JsonReader(new ByteArrayInputStream(bytes));
-                }
-                else
-                {
-                    reader = new JsonReader(IOUtilities.getInputStream(c));
-                }
-                Object[] res = (Object[]) reader.readObject();
+                reader = new InputStreamReader(IOUtilities.getInputStream(c));
+                Gson gson = new Gson();
+                Object[] res = gson.fromJson(reader, Object[].class);
                 return res[0];
             }
             finally

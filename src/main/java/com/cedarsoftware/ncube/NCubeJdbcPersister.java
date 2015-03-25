4a114b02a3d32b4073b0d6eca5e5e4ea8648eb4c
kpartlow <kpartlow@gmail.com>
2015-03-25 17:13:47 -0400

    Updated rename to not have conflict on a rename back to a renamed cube

--- src/main/java/com/cedarsoftware/ncube/NCubeJdbcPersister.java
+++ src/main/java/com/cedarsoftware/ncube/NCubeJdbcPersister.java
@@ -1431,6 +1431,13 @@ public class NCubeJdbcPersister
                         byte[] notes = StringUtilities.getBytes("Cube renamed:  " + oldName + " -> " + newName, "UTF-8");
 
 
+                        Long newRevision = getMaxRevision(c, appId, newName);
+
+                        if (newRevision == null)
+                        {
+                            newRevision = 0L;
+                        }
+
                         try (PreparedStatement insert = c.prepareStatement("INSERT INTO n_cube (n_cube_id, app_cd, n_cube_nm, cube_value_bin, version_no_cd, create_dt, create_hid, tenant_cd, branch_id, revision_number, test_data_bin, notes_bin) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"))
                         {
                             insert.setLong(1, UniqueIdGenerator.getUniqueId());
@@ -1442,7 +1449,7 @@ public class NCubeJdbcPersister
                             insert.setString(7, username);
                             insert.setString(8, appId.getTenant());
                             insert.setString(9, appId.getBranch());
-                            insert.setLong(10, 0);
+                            insert.setLong(10, newRevision == null ? 0 : Math.abs(revision) + 1);
                             insert.setBytes(11, testData);
                             insert.setBytes(12, notes);
 

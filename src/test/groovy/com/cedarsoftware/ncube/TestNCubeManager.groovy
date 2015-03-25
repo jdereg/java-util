4af4ceaf862603f746bde75d5f5f1dc3e624fc02
jdereg <jdereg@gmail.com>
2015-03-25 08:15:03 -0400

    - Changed 'error' to 'conflict'

--- src/test/groovy/com/cedarsoftware/ncube/TestNCubeManager.groovy
+++ src/test/groovy/com/cedarsoftware/ncube/TestNCubeManager.groovy
@@ -14,7 +14,12 @@ import org.junit.Test
 import java.nio.file.Files
 import java.nio.file.Paths
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertNull
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.fail
 
 /**
  * NCubeManager Tests

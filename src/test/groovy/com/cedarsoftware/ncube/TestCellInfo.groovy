de5afa0ba9e0e3080a3d612d15c59329618b04c2
John DeRegnaucourt <john@cedarsoftware.com>
2015-05-12 23:23:21 -0400

    - Split the merge API into two steps to aid the branch merge function. First step, getCellDelta() which returns a Map of the differences between two cubes.  Second step, merge(deltas) which 'plays' the deltas into the cube, effecting the change.

--- src/test/groovy/com/cedarsoftware/ncube/TestCellInfo.groovy
+++ src/test/groovy/com/cedarsoftware/ncube/TestCellInfo.groovy
@@ -6,7 +6,12 @@ import com.cedarsoftware.ncube.proximity.Point3D
 import com.cedarsoftware.util.io.JsonObject
 import org.junit.Test
 
-import static org.junit.Assert.*
+import static org.junit.Assert.assertArrayEquals
+import static org.junit.Assert.assertEquals
+import static org.junit.Assert.assertFalse
+import static org.junit.Assert.assertNull
+import static org.junit.Assert.assertTrue
+import static org.junit.Assert.fail
 
 /**
  * @author John DeRegnaucourt (jdereg@gmail.com)
@@ -218,28 +223,51 @@ class TestCellInfo
     }
 
     @Test(expected = IllegalArgumentException.class)
-    void testParseJsonValueException() throws Exception
+    void testParseJsonValueException()
     {
         CellInfo.parseJsonValue(Boolean.TRUE, "http://www.foo.com", "foo", true)
     }
 
     @Test(expected = IllegalArgumentException.class)
-    void testParseJsonValueNonUrlException() throws Exception
+    void testParseJsonValueNonUrlException()
     {
         CellInfo.parseJsonValue("blah blah blah", null, "foo", true)
     }
 
     @Test(expected = IllegalArgumentException.class)
-    void testParseJsonValueWithUnknownType() throws Exception
+    void testParseJsonValueWithUnknownType()
     {
         CellInfo.parseJsonValue(new Object(), null, "foo", true)
     }
 
     @Test
-    void testParseJsonValueGroovyMethod() throws Exception
+    void testParseJsonValueGroovyMethod()
     {
         GroovyMethod method = (GroovyMethod) CellInfo.parseJsonValue("def [5]", null, "method", true)
         assertEquals(new GroovyMethod("def [5]", null, false), method)
+    }
+
+    @Test
+    void testEqualsAndHashCode()
+    {
+        CellInfo info1 = new CellInfo([1, 2, 4, 8, 16] as byte[])
+        CellInfo info2 = new CellInfo([1, 2, 4, 8, 16] as byte[])
+        assert info1 == info2
+        assert info1.hashCode() == info2.hashCode()
+        info2 = new CellInfo([1, 2, 4, 8, 16, 32] as byte[])
+        assert info1 != info2
+        assert info1.hashCode() != info2.hashCode()
+
+        info1 = new CellInfo(1.33d)
+        info2 = new CellInfo(1.33f)
+        assert info1 != info2
+        assert info1.hashCode() != info2.hashCode()
+
+        def s = "Hi"
+        info1 = new CellInfo(s)
+        assert info1 == info1
+
+        assertFalse info1.equals(13)
     }
 
     public void performRecreateAssertion(Object o)

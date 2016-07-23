e8e879b6418ecd3a497db71d4c477d1d9a37705d
John DeRegnaucourt <jdereg@gmail.com>
2016-07-23 16:08:09 -0400

    - Added missing copyright headers

--- src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWhenExceptionsAreThrown.java
+++ src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWhenExceptionsAreThrown.java
@@ -21,6 +21,24 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+/**
+ * @author Ken Partlow
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
 @PowerMockIgnore("javax.management.*")
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({UrlUtilities.class})
--- src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWithPlainReader.java
+++ src/test/java/com/cedarsoftware/util/TestUrlInvocationHandlerWithPlainReader.java
@@ -1,8 +1,5 @@
 package com.cedarsoftware.util;
 
-//import com.cedarsoftware.util.io.JsonReader;
-//import com.cedarsoftware.util.io.JsonWriter;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
@@ -17,9 +14,22 @@ import java.net.URL;
 import java.net.URLConnection;
 
 
-
 /**
- * Created by kpartlow on 5/11/2014.
+ * @author Ken Partlow
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
  */
 public class TestUrlInvocationHandlerWithPlainReader
 {

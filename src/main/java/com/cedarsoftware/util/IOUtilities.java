1dbc225ac6abb84c7f2b3a966b036f2329b18815
John DeRegnaucourt <jdereg@gmail.com>
2017-09-05 09:26:25 -0400

    - added channel-based copy to IOUtilities - removed 'release' from .travis.yml

--- src/main/java/com/cedarsoftware/util/IOUtilities.java
+++ src/main/java/com/cedarsoftware/util/IOUtilities.java
@@ -16,6 +16,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLConnection;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
@@ -335,5 +339,25 @@ public final class IOUtilities
         void bytesTransferred(byte[] bytes, int count);
 
         boolean isCancelled();
+    }
+
+    public static long copy(InputStream input, OutputStream output) throws IOException
+    {
+        try (
+                ReadableByteChannel inputChannel = Channels.newChannel(input);
+                WritableByteChannel outputChannel = Channels.newChannel(output); )
+        {
+            ByteBuffer buffer = ByteBuffer.allocateDirect(10240);
+            long size = 0;
+
+            while (inputChannel.read(buffer) != -1)
+            {
+                buffer.flip();
+                size += outputChannel.write(buffer);
+                buffer.clear();
+            }
+
+            return size;
+        }
     }
 }

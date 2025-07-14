641341e333afd3fe0f9d55155b11ee094fe7e02a
Claude4.0s <claude4.0s@ai.assistant>
2025-07-14 19:13:36 -0400

    ai protection

--- src/test/java/com/cedarsoftware/util/MultiKeyMapTest.java
+++ src/test/java/com/cedarsoftware/util/MultiKeyMapTest.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MultiKeyMapTest {
     @Test
     void testSingleElementArrayKeys() {
-        MultiKeyMap<String> map = new MultiKeyMap<>(MultiKeyMap.CollectionKeyMode.COLLECTIONS_EXPANDED, true);
+        MultiKeyMap<String> map = new MultiKeyMap<>(true);
 
         map.put("a", "alpha");
         map.put(new String[]{"a"}, "[alpha]");
@@ -30,10 +30,60 @@ public class MultiKeyMapTest {
         map.remove("a");
         assert map.isEmpty();
         
+        map.putMultiKey("alpha", "a");
+        map.putMultiKey("[alpha]", (Object) new String[]{"a"});
+        map.putMultiKey("[[alpha]]", (Object) new String[][]{{"a"}});
+        map.putMultiKey("[[[alpha]]]", (Object) new String[][][]{{{"a"}}});
+
+        map.removeMultiKey("a");
+        assert map.isEmpty();
+
         map.put("a", "alpha");
         map.put(new String[]{"a"}, "[alpha]");
         map.put(new String[][]{{"a"}}, "[[alpha]]");
         map.put(new String[][][]{{{"a"}}}, "[[[alpha]]]");
+
+        map.remove(new String[][][]{{{"a"}}});
+        assert map.isEmpty();
+
+        map.put("a", "alpha");
+        map.put(new String[]{"a"}, "[alpha]");
+        map.put(new String[][]{{"a"}}, "[[alpha]]");
+        map.put(new String[][][]{{{"a"}}}, "[[[alpha]]]");
+
+        map.removeMultiKey((Object) new String[][][]{{{"a"}}});
+        assert map.isEmpty();
+    }
+
+    @Test
+    void testSingleElementArrayKeysInCaseInsensitiveMap() {
+        CaseInsensitiveMap<Object, String> map = new CaseInsensitiveMap<>(new MultiKeyMap<>(true));
+
+        map.put("a", "alpha");
+        map.put(new String[]{"a"}, "[alpha]");
+        map.put(new String[][]{{"a"}}, "[[alpha]]");
+        map.put(new String[][][]{{{"a"}}}, "[[[alpha]]]");
+
+        assert map.size() == 1;
+        assertEquals("[[[alpha]]]", map.get("a"));      // last put
+
+        assert map.containsKey("a");
+        assert map.containsKey(new String[]{"a"});
+        assert map.containsKey(new String[][]{{"a"}});
+        assert map.containsKey(new String[][][]{{{"a"}}});
+
+        assert map.containsMultiKey("a");
+        assert map.containsMultiKey((Object) new String[]{"a"});
+        assert map.containsMultiKey((Object) new String[][]{{"a"}});
+        assert map.containsMultiKey((Object) new String[][][]{{{"a"}}});
+
+        map.remove("a");
+        assert map.isEmpty();
+
+        map.putMultiKey("alpha", "a");
+        map.putMultiKey("[alpha]", (Object) new String[]{"a"});
+        map.putMultiKey("[[alpha]]", (Object) new String[][]{{"a"}});
+        map.putMultiKey("[[[alpha]]]", (Object) new String[][][]{{{"a"}}});
 
         map.removeMultiKey("a");
         assert map.isEmpty();
